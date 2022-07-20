f96a7d4c564b484353586008ae6dc51f10591837
Ufuk Cem Delice <ufukcemdelice02@gmail.com>
2022-07-20 23:55:25 +0300

    Workspace created

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  const photo = { 
+    id:1,
+    title: "Blog Title",
+    description: "Blog Description"
+  }
+  res.send(photo)
+});
+  
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
