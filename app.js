fc961471ef78a9769361b19f169edccbdb40dfd3
Ufuk Cem Delice <ufukcemdelice02@gmail.com>
2022-07-23 00:53:53 +0300

    Static Files has been uploaded

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
-const port = 3000;
+//Template Methot
+app.set('view engine', 'ejs');
+
+//MiddleWares
+app.use(express.static('public'));
+//Routes
 app.get('/', (req, res) => {
-  const photo = { 
-    id:1,
-    title: "Blog Title",
-    description: "Blog Description"
-  }
-  res.send(photo)
+  res.render('index');
 });
-  
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
