fa18f179d838790d31a62844685e537e323595fa
Ufuk Cem Delice <ufukcemdelice02@gmail.com>
2022-07-29 00:19:57 +0300

    Db has been connected, middleware etc has done

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const Post = require('./models/Post');
+const mongoose = require('mongoose');
 const app = express();
-//Template Methot
+
+//Db connection
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Template Method
 app.set('view engine', 'ejs');
 
 //MiddleWares
-app.use(express.static('public'));
+app.use(express.static((path.resolve(__dirname + '/public'))));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  console.log(posts)
+  res.render('index', {posts});
+
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
+
 app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
+
+
 app.get('/post', (req, res) => {
   res.render('post');
+});
+
+app.post('/add_post', async (req, res) => {
+  await Post.create(req.body)
+  res.redirect('/')
 });
 
 const port = 3000;
